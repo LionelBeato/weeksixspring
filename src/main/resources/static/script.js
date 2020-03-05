4783b4681572f355c558d0e00d937087028e90e5
LionelBeato <lionel.beato@gmail.com>
2020-03-05 14:14:01 -0500

    adds working put method

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -17,45 +17,74 @@ fetch('http://localhost:8080/donuts')
             // console.log(json[el].name)
             let tbody = document.querySelector("tbody")
             let tr = document.createElement("tr")
+            tr.setAttribute("contenteditable", "false")
             tr.classList = `row-${json[el].id} item`
             tr.innerHTML = `<td>${json[el].id}</td>
-            <td>${json[el].name}</td>
-            <td>${json[el].frosted ? 'frosted' : 'not frosted'}</td>
-            <td>${json[el].donutType}</td>
+            <td id="${json[el].id}-name">${json[el].name}</td>
+            <td id="${json[el].id}-frosted">${json[el].frosted}</td>
+            <td id="${json[el].id}-type">${json[el].donutType}</td>
             <td><i class="fa fa-trash icon-${json[el].id}" style="cursor:pointer" onclick="deleteItem(${json[el].id})"></i></td>
-            <td><i class="fas fa-pencil-alt icon-${json[el].id}" onclick="updateItem()"></i></td>`
+            <td><i class="fas fa-pencil-alt icon-${json[el].id}" style="cursor:pointer" onclick="updateItem(${json[el].id})"></i></td>
+            <td><i class="fas fa-save icon-${json[el].id}" style="cursor:pointer" onclick="saveItem(${json[el].id})"></i></td>`
             tbody.appendChild(tr)
         }
     })
 
 
 
-    // delete method that is tied to the trashcan icon
+// delete method that is tied to the trashcan icon
 
-    const deleteItem = (id) => {
-        let item = document.querySelector(`.row-${id}`)
-       fetch(`http://localhost:8080/donut/${id}`, {
-           method: 'DELETE',
-       })
-       .then(item.remove())
-       .then(console.log(`this works`))
-       
+const deleteItem = (id) => {
+    let item = document.querySelector(`.row-${id}`)
+    fetch(`http://localhost:8080/donut/${id}`, {
+        method: 'DELETE',
+    })
+        .then(item.remove())
+        .then(console.log(`this works`))
 
+
+}
+
+// update method that will be tied to pencil icon
+
+const updateItem = (id) => {
+    let item = document.querySelector(`.row-${id}`)
+    item.toggleAttribute("contenteditable")
+
+}
+
+const saveItem = (id) => {
+    let nameInput = document.getElementById(`${id}-name`).innerText
+    let frostedInput = document.getElementById(`${id}-frosted`).innerText 
+    let typeInput = document.getElementById(`${id}-type`).innerText
+
+    let sampleDonut = {
+        name: nameInput,
+        frosted: frostedInput,
+        donutType: typeInput
     }
 
-    // update method that will be tied to pencil icon
+    console.log(sampleDonut)
 
-    const updateItem = () => {
-        console.log("update works")
-    }
+    fetch(`http://localhost:8080/donut/${id}`, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: 'PUT',
+        body: JSON.stringify(sampleDonut)
+    })
+    .then(response => response.json)
+    .then(json => console.log(json))
+}
 
 
 
 
-    // this add method works completely fine! 
+// this add method works completely fine! 
 
 
-    const addItem = () => {
+const addItem = () => {
 
     let nameInput = document.getElementById("name").value
     let frostedInput = document.getElementById("frosted").value
@@ -63,21 +92,21 @@ fetch('http://localhost:8080/donuts')
 
 
     let sampleDonut = {
-            name: nameInput,
-            frosted: frostedInput,
-            donutType: typeInput
+        name: nameInput,
+        frosted: frostedInput,
+        donutType: typeInput
     }
 
-        console.log(sampleDonut)
-        fetch(`http://localhost:8080/donut`, {
-            headers: { 
-                'Accept': 'application/json',
-                'Content-Type': 'application/json' 
-            },
-            method: 'POST',
-            body: JSON.stringify(sampleDonut)
-        
-        }).then(console.log("cake posted!"))
+    console.log(sampleDonut)
+    fetch(`http://localhost:8080/donut`, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(sampleDonut)
+
+    }).then(console.log("cake posted!"))
         .then(location.reload())
 
-    }
+}
